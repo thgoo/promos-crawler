d9464fd00cceae42f336793b16a18b6d0d7ea396
thgoo <thiagoeu@gmail.com>
2026-03-31 13:47:02 -0300

    fix tecnoblog links

--- src/affiliates/constants.ts
+++ src/affiliates/constants.ts
@@ -29,9 +29,14 @@ export const PROMOZONE_RESOLVE_API = 'https://link-shortener-501307668672.southa
 export const HTTP_HEADERS = {
   // eslint-disable-next-line @stylistic/max-len
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
   'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-  'Accept-Encoding': 'gzip, deflate',
+  'Accept-Encoding': 'gzip, deflate, br',
   'Connection': 'keep-alive',
   'Cache-Control': 'max-age=0',
+  'Upgrade-Insecure-Requests': '1',
+  'Sec-Fetch-Dest': 'document',
+  'Sec-Fetch-Mode': 'navigate',
+  'Sec-Fetch-Site': 'none',
+  'Sec-Fetch-User': '?1',
 };
